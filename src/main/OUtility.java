f166946eaa7f20e779c87f0ba208b10f172294c5
McFeelio <owenjmcneil@gmail.com>
2016-02-22 18:32:19 -0500

    Removed password requirements. We're all friends here.

--- src/main/OUtility.java
+++ src/main/OUtility.java
@@ -6,27 +6,14 @@ public class OUtility {
 		
 	}
 	
-	/**
-	 * This method is required to create a new instance of the ObjectManager class.
-	 * @param passkey The passkey that was (hopefully) given to you by Owen.
-	 * @return If the passkey is correct, this method returns a new instance of the ObjectManager. Otherwise, it returns null.
-	 */
-	public static OUtility Authorize(String passkey) {
-		
-		if (passkey.equals("shaco op")) {
-			OUtility utility = new OUtility();
-			return utility;
-		} else {
-			return null;
-		}
-	}
+
 	
 	/**
 	 * This method creates a new Object Manager, which can be used to manipulate objects in several different, useful ways!
 	 * @return Returns a new instance of the OUtility ObjectManager class.
 	 */
 	public ObjectManager createObjectManager() {
-		ObjectManager manager = ObjectManager.Authorize("ahdkajhdjadahsd723173128372183kdjasd@#^#&#*&#hjsdkshdady28");
+		ObjectManager manager = new ObjectManager();
 		return manager;
 	}
 
